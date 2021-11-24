db2b4d1fa5aa8a77ba685904d351a2fb9492d750
CalebPfaff <kalmarv@pm.me>
2021-11-23 17:10:58 -0800

    Random Sens Ported

--- src/Random.svelte
+++ src/Random.svelte
@@ -1,7 +1,74 @@
-<div>
-    <h1>Generate a Random Sensitivity</h1>
-    <p>Try 6 haha</p>
-</div>
+<script>
+let games_dic = {
+    Overwatch: [0.0066, 2],
+    Fortnite: [0.005555, 3],
+    CounterStrike: [0.022, 2],
+    QuakeChampions: [0.022, 6],
+};
 
-<style>
-</style>
+let value = "Overwatch";
+let items = ["Overwatch", "Fortnite", "CounterStrike", "QuakeChampions"];
+let sens = 6;
+let dpi = 800;
+let lower = 20;
+let higher = 40;
+
+let yaw = 0.0066;
+let precision = 2;
+
+let randNumRounded = 28.86
+
+function setGame() {
+    for (const [key, values] of Object.entries(games_dic)) {
+        if (key == value) {
+            yaw = values[0];
+            precision = values[1];
+        }
+    }
+}
+
+function genSens() {
+    let randNum =
+        Number(lower) + Math.random() * (Number(higher) - Number(lower));
+    let unrounded = 4572.0 / (5.0 * randNum * Number(dpi) * yaw);
+    sens = Number.parseFloat(unrounded).toFixed(precision);
+    randNumRounded = Number.parseFloat(realCM(sens)).toFixed(2)
+}
+
+function realCM(inputSens) {
+    return 914.4 / (inputSens * dpi * yaw)
+}
+</script>
+
+<main>
+    <div>
+        <h1>Random Sensitivity Generator</h1>
+    </div>
+    <div>
+        <div>
+            <h3>Generate Random Sensitivity for:</h3>
+            <select bind:value on:change={setGame}>
+                {#each items as item}
+                <option value={item}>{item}</option>
+                {/each}
+            </select>
+        </div>
+        <div>
+            <p>DPI:</p>
+            <input type="text" bind:value={dpi} size="1" />
+            <p>Min:</p>
+            <input type="text" bind:value={lower} size="1" />
+            <p>Max:</p>
+            <input type="text" bind:value={higher} size="1" />
+        </div>
+        <div>
+            <button on:click={genSens}>Generate Sensitivity</button>
+        </div>
+        <div>
+            <p>Your sensitivity is: {sens}</p>
+            <p>({randNumRounded} cm/360)</p>
+        </div>
+    </div>
+</main>
+
+<style></style>
