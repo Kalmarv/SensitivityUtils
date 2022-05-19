2570c58e6f138c343fd8035cdc4df49da81118aa
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-18 21:51:40 -0600

    ✨feature: Better styling, still some to go though

--- src/components/Current.svelte
+++ src/components/Current.svelte
@@ -17,22 +17,42 @@
   $: selectedGame, inputDPI, inputSens, displaySens()
 </script>
 
-<h1 class="text-center">Calculate CM/360</h1>
+<h1 class="text-center text-4xl m-8">Calculate CM/360</h1>
 
-<select bind:value={inputGameID} class="select select-bordered w-full max-w-xs m-4">
-  {#each gamesList as game}
-    <option value={game.id}>{game.name}</option>
-  {/each}
-</select>
+<div class="flex flex-col lg:flex-row mx-auto my-4">
+  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8">
+    <!--  -->
+    <select bind:value={inputGameID} class="select select-bordered w-full max-w-xs my-2">
+      {#each gamesList as game}
+        <option value={game.id}>{game.name}</option>
+      {/each}
+    </select>
+    <div class="form-control my-2">
+      <label class="input-group">
+        <span>In-Game Sens</span>
+        <input bind:value={inputDPI} type="number" placeholder="Sensitivity" class="input input-bordered" />
+      </label>
+    </div>
 
-<input bind:value={inputDPI} type="number" placeholder="Sensitivity" class="input input-bordered w-full max-w-xs m-4" />
-<input bind:value={inputSens} type="number" placeholder="DPI" class="input input-bordered w-full max-w-xs m-4" />
-
-<div class="stats shadow">
-  <div class="stat place-items-center">
-    <div class="stat-title">Your Sensitivity</div>
-    <div class="stat-value text-primary">{`${displayedSens} CM/360`}</div>
-    <div class="stat-desc">{`In-Game Sens: ${inputSens}`}</div>
-    <div class="stat-desc">{`DPI: ${inputDPI}`}</div>
+    <div class="form-control my-2">
+      <label class="input-group">
+        <span>DPI</span>
+        <input bind:value={inputSens} type="number" placeholder="DPI" class="input input-bordered" />
+      </label>
+    </div>
+    <!--  -->
+  </div>
+  <div class="divider lg:divider-horizontal" />
+  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8">
+    <!--  -->
+    <div class="stats shadow">
+      <div class="stat place-items-center">
+        <div class="stat-title">Your Sensitivity</div>
+        <div class="stat-value text-primary">{`${displayedSens} CM/360`}</div>
+        <div class="stat-desc">{`In-Game Sens: ${inputSens}`}</div>
+        <div class="stat-desc">{`DPI: ${inputDPI}`}</div>
+      </div>
+    </div>
+    <!--  -->
   </div>
 </div>
