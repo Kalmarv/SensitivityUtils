f4d81c87d45d63c53e3df752b3611f7fa5eadd56
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-18 23:33:52 -0600

    ✨feature: Add shadows cause it looks cool

--- src/components/Current.svelte
+++ src/components/Current.svelte
@@ -20,22 +20,22 @@
 <h1 class="text-center text-4xl m-8">Calculate CM/360</h1>
 
 <div class="flex flex-col lg:flex-row mx-auto my-4">
-  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8">
+  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8 shadow-lg">
     <!--  -->
-    <select bind:value={inputGameID} class="select select-bordered w-full max-w-xs my-2">
+    <select bind:value={inputGameID} class="select select-bordered w-full max-w-xs my-2 shadow-md">
       {#each gamesList as game}
         <option value={game.id}>{game.name}</option>
       {/each}
     </select>
     <div class="form-control my-2">
-      <label class="input-group">
+      <label class="input-group shadow-md">
         <span>In-Game Sens</span>
         <input bind:value={inputSens} type="number" placeholder="Sensitivity" class="input input-bordered" />
       </label>
     </div>
 
     <div class="form-control my-2">
-      <label class="input-group">
+      <label class="input-group shadow-md">
         <span>DPI</span>
         <input bind:value={inputDPI} type="number" placeholder="DPI" class="input input-bordered" />
       </label>
@@ -43,9 +43,9 @@
     <!--  -->
   </div>
   <div class="divider lg:divider-horizontal" />
-  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8">
+  <div class="grid flex-grow card bg-base-200 rounded-box place-items-center p-8 shadow-lg">
     <!--  -->
-    <div class="stats shadow">
+    <div class="stats shadow-lg">
       <div class="stat place-items-center">
         <div class="stat-title">Your Sensitivity</div>
         <div class="stat-value text-primary mb-2">{`${displayedSens} CM/360`}</div>
