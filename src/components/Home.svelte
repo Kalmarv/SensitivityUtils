bebb166c1bfa55d9f4d8f0cfbc9bf2fea50df194
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-19 19:25:35 -0600

    ✨feature: Better home page

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,1 +1,15 @@
-<h1 class="text-center">Hi!</h1>
+<div class="hero">
+  <div class="hero-content text-center mt-16">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hey There!</h1>
+      <p class="py-6">
+        I wrote some tools to help you convert sensitivity values between different games, generate random sensitivities
+        and more, check it out.
+      </p>
+      <p>
+        If you're interested in how it was built, or want to give it a ⭐, check it out on
+        <a href="https://github.com/Kalmarv/Synify/issues" class="link">GitHub</a>
+      </p>
+    </div>
+  </div>
+</div>
