c92185344e2c517b8f9903294fbe8e7b914fa048
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-06-03 18:02:00 -0600

    🎨 format tests

--- tests/e2e/random.spec.ts
+++ tests/e2e/random.spec.ts
@@ -1,31 +1,14 @@
 import { test, expect } from '@playwright/test'
 
 test('Generate Random Sens', async ({ page }) => {
-  // Go to http://localhost:3000/#/random
   await page.goto('http://localhost:3000/#/random')
-
-  // Select 2
   await page.locator('select').selectOption('2')
-
-  // Click [placeholder="DPI"]
   await page.locator('[placeholder="DPI"]').click()
-
-  // Fill [placeholder="DPI"]
   await page.locator('[placeholder="DPI"]').fill('800')
-
-  // Click [placeholder="Minimum CM\/360"]
   await page.locator('[placeholder="Minimum CM\\/360"]').click()
-
-  // Fill [placeholder="Minimum CM\/360"]
   await page.locator('[placeholder="Minimum CM\\/360"]').fill('20')
-
-  // Click [placeholder="Maximum CM\/360"]
   await page.locator('[placeholder="Maximum CM\\/360"]').click()
-
-  // Fill [placeholder="Maximum CM\/360"]
   await page.locator('[placeholder="Maximum CM\\/360"]').fill('40')
-
-  // Click text=Regenerate
   await page.locator('text=Regenerate').click()
 
   const sens = page.locator('id=sens')
